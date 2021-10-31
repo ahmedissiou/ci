9df4d2ee51b84442c9e3c87edd63e09c9fb19731
selimaouini <mohamedselim.aouini@esprit.tn>
2021-11-01 00:46:24 +0100

    log

--- src/test/java/tn/esprit/spring/service/ContratServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/ContratServiceImplTest.java
@@ -62,7 +62,7 @@ public class ContratServiceImplTest {
 			
 			@Test
 			public void testDeleteContrat() {
-				 us.deleteContrat(39);
+				 us.deleteContrat(40);
 				//Assert.assertNull(us.retrieveContrat(7));
 				
 			}
