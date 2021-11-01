e549bb7c03c37716663b0ef85b5dac8d0d46de2f
Issiou Ahmed <61413652+ahmedissiou@users.noreply.github.com>
2021-11-01 09:37:35 +0000

    Update IUserService.java

--- src/main/java/tn/esprit/spring/services/IUserService.java
+++ src/main/java/tn/esprit/spring/services/IUserService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import tn.esprit.spring.entities.User;
 
 public interface IUserService { 
-	 
+	 //hello
 	List<User> retrieveAllUsers(); 
 	User addUser(User u);
 	void deleteUser(String id);
